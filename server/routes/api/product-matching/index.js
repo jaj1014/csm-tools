785d402d0e7277620e04cd2057caf20146a48c19
Alex Jackson <jaj1014@gmail.com>
2017-06-21 13:11:14 -0400

    built out logic for parsing errors and returning file to download

--- server/routes/api/product-matching/index.js
+++ server/routes/api/product-matching/index.js
@@ -3,14 +3,14 @@ const router = express.Router()
 const multer = require('multer')
 const upload = multer({dest: './uploads'})
 
-// const buildJSON = require('../../../utils/build-json.js')
-// const buildCampaign = require('../../../utils/build-campaign.js').buildCampaign
-// const deleteFiles = require('../../../utils/delete-files.js')
+const parseMatchingErrors = require('../../../utils/parse-matching-errors.js').buildFile
+const deleteFiles = require('../../../utils/delete-files.js')
 
 router.post('/:action', upload.single('upload'), (req, res) => {
   switch (req.params.action) {
     case 'upload':
-      res.json('it is working')
+      deleteFiles('./public/downloads')
+      res.json(parseMatchingErrors(req.file, req.file.filename))
       break
   }
 })
--- server/utils/parse-matching-errors.js
+++ server/utils/parse-matching-errors.js
@@ -1,3 +1,70 @@
+const fs = require('fs')
+const json2csv = require('json2csv')
+const buildJSON = require('./build-json.js')
+
+const fields = [
+  'SKU',
+  'Title',
+  'Field Name',
+  'Merchant Value',
+  'Amazon Value'
+]
+
+const buildFile = (file, fileName) => {
+  let file = []
+  let matchingData = buildMatchingData(obj)
+  let fileName = fileName.split('/')[2]
+
+  matchingData.forEach((item) => {
+    item.error.map((err) => {
+      file.push({
+        'SKU': item.sku,
+        'Title': item.title,
+        'Field Name': err.field,
+        'Merchant Value': err.merchant,
+        'Amazon Value': err.amazon
+      })
+    })
+  })
+
+  const newFile = json2csv({data: file, fields: fields})
+  fs.writeFileSync(`./public/downloads/${fileName}.csv`, newFile)
+  return `/downloads/${fileName}.csv`
+}
+
+// no ideal... need to figure this out...
+// const newLine = (obj) => {
+//   return {
+//     'SKU': obj.sku,
+//     'Title': obj.title,
+//     'Field Name': obj.error.field,
+//     'Merchant Value': obj.error.merchant,
+//     'Amazon Value': obj.error.amazon
+//   }
+// }
+
+const buildMatchingData = (obj) => {
+  let file = []
+  let fileJSON = jsonFromFile(obj.file)
+
+  file = fileJSON.map((row) => {
+    return matchingErrorData(row)
+  })
+
+  return file
+}
+
+const matchingErrorData = (row) => {
+  let skuError = {}
+  let errorStr = getTextFromString(row['Error Details'])
+
+  skuError.sku = row['SKU']
+  skuError.title = row['Title']
+  skuError.error = separateErrors(errorStr)
+
+  return skuError
+}
+
 const getTextFromString = (str) => {
   return str.match(/[^\]]*$/)[0]
      .split(',')
@@ -15,8 +82,21 @@ const separateErrors = (arr) => {
   })
 }
 
+const jsonFromFile = (filePath) => {
+  let lines = buildJSON.parseFile(filePath)
+  let headers = buildJSON.getHeaders(lines)
+  let fileJSON = buildJSON.compileJSON(lines, headers)
+
+  return fileJSON
+}
+
+module.exports = {
+  buildFile: buildFile
+}
+
 // [{
 //   sku: 'theSKU',
+//   title: 'title of product',
 //   error: [
 //     {field: 'Brand', merchant: 'value', amazon: 'value'},
 //     {field: 'Brand', merchant: 'value', amazon: 'value'}
