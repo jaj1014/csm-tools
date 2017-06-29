29ddf8bec7e9bfc72ee9ca619ed3a00eb7602b14
Alex Jackson <jaj1014@gmail.com>
2017-06-29 13:46:45 -0400

    fixed typos

--- js/Main/ProductMatching/ProductMatchingUpload.jsx
+++ js/Main/ProductMatching/ProductMatchingUpload.jsx
@@ -6,7 +6,7 @@ const ProductMatchingUpload = (props) => {
     actionUrl={'/api/product-matching/upload'}
     handleSubmit={props.handleSubmit}
     title='Select a file to upload'
-    description='Make sure your tile is in .txt format and that it has all columns labeled. Some files leave out an ASIN column. Shift column headers to the right to insert a column named "ASIN".'
+    description='Make sure your file is in .txt format and that it has all columns labeled. Some files leave out an ASIN column. Shift column headers to the right to insert a column named "ASIN".'
   />
 }
 
